2d377015a51b776a075bf0d6ccb00da77587bed2
sianghaochin <shchinwork@gmail.com>
2023-06-07 21:47:27 +0800

    Update Footer

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -1,8 +1,6 @@
 import {ChevronUpIcon} from '@heroicons/react/24/outline';
 import {FC, memo, useEffect,useState} from 'react';
 
-import {SectionId} from '../../data/data';
-
 const getCurrentYear = () => {
   return new Date().getFullYear();
 };
@@ -21,7 +19,7 @@ export const Footer: FC = memo(() => {
       <div className="absolute inset-x-0 -top-4 flex justify-center sm:-top-6">
         <a
           className="rounded-full bg-neutral-100 p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2"
-          href={`/shchin_portfolio/#${SectionId.Hero}`}
+          href="/shchin_portfolio/#hero"
         >
           <ChevronUpIcon className="h-6 w-6 bg-transparent sm:h-8 sm:w-8" />
         </a>
